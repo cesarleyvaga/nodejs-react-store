94c6e4a76af11a4b8e8ab44d803c34efe0fd725c
cesarleyvaga <cesar_leyva@outlook.com>
2026-05-07 14:13:27 -0600

    init: initial project setup

---
+++ backend/src/server.js
@@ -0,0 +1,7 @@
+const app = require("./app");
+const env = require("./config/env");
+const PORT = env.port;
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost" + PORT);
+});
