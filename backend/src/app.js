519750bda0b49e43d67c744fe1d39c75f594debe
cesarleyvaga <cesar_leyva@outlook.com>
2026-09-24 00:10:45 -0600

    feat(users): add user routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const productRoutes = require("./modules/products/products.index");
-const errorMiddleware = require("./middlewares/error.middleware.js");
+const userRoutes = require("./modules/users/users.index");
+const errorMiddleware = require("./middlewares/error.middleware");
 const notFoundMiddleware = require("./middlewares/notFound.middleware");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
 
